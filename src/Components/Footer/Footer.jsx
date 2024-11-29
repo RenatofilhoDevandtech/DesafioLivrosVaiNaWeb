d408cbd6027af17ad43814148b1be4ec9d580411
RenatoFilho <https://github.com/RenatofilhoDevandtech>
2024-11-29 17:38:30 -0300

    � Atualiza estilos e funcionalidade do Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,39 +4,35 @@ import IconTwitter from '../../assets/IconTwitter.png';
 import Iconyoutube from '../../assets/Iconyoutube.png';
 import Iconlinkedin from '../../assets/Iconlinkedin.png';
 import Iconinstagram from '../../assets/Iconinstagram.png';
-// Importa o arquivo de estilos específico para este componente
 import s from './Footer.module.scss';
 
 export default function Footer() {
   return (
     <footer className={s.footer}>
-      <div className={s.container}>
-        <div className={s.footerContent}>
-          {/* Número de telefone alinhado à esquerda */}
+      
+        <section className={s.footerContent}>
           <p className={s.footerPhone}>4002-8922</p>
-          {/* Links das redes sociais alinhados à direita */}
-          <div className={s.footerSocialLinks}>
+            <nav className={s.footerSocialLinks}>
             <a href="#" aria-label="Facebook">
-              <img src={Iconfacebook} alt="Facebook" />
+              <img src={Iconfacebook} alt="imagem na cor branca da logo Facebook" />
             </a>
             <a href="#" aria-label="Twitter">
-              <img src={IconTwitter} alt="Twitter" />
+              <img src={IconTwitter} alt="imagem na cor branca da logo Twitter" />
             </a>
             <a href="#" aria-label="YouTube">
-              <img src={Iconyoutube} alt="YouTube" />
+              <img src={Iconyoutube} alt="imagem na cor branca da logo YouTube" />
             </a>
             <a href="#" aria-label="LinkedIn">
-              <img src={Iconlinkedin} alt="LinkedIn" />
+              <img src={Iconlinkedin} alt="imagem na cor branca da logo LinkedIn" />
             </a>
             <a href="#" aria-label="Instagram">
-              <img src={Iconinstagram} alt="Instagram" />
+              <img src={Iconinstagram} alt="imagem na cor branca da logo Instagram" />
             </a>
-          </div>
-        </div>
-        <div className={s.footerFim}>
+            </nav>
+        </section>
+        <section className={s.footerFim}>
           <p className={s.footerCopyright}>&copy; 2024 - Layout desenvolvido pela Vai na Web para fins educacionais</p>
-        </div>
-      </div>
+        </section>
     </footer>
   );
 }
